e2d629039796a1a09b1170e640ca74b1ffaece91
oleksiiKAV <oleksii.kav@gmail.com>
2023-12-14 10:41:50 +0200

    init

---
+++ OK/CSharpBasic/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int a = 12;
+        Console.WriteLine("number is " + a);
+        string s = "It is a string";
+        Console.WriteLine(s);
+        Console.WriteLine($"String is: {s}");
+        var v = 24;
+        Console.WriteLine($"It is var value = {v}");
+        dynamic height = 13.2;
+        
+    }
+}
